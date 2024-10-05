4f7c4c20ac25056b72a4051b1ca50b3734fe4e04
Dragon1573 <49941141+Dragon1573@users.noreply.github.com>
2024-10-05 21:50:05 +0800

    AutoTencent.py: 优化代码并更新说明
    - 从 Google Chrome 切换为 Microsoft Edge
    - 针对腾讯文档 Web 端改版，优化元素定位和自动控制逻辑
    
    Signed-off-by: Dragon1573 <49941141+Dragon1573@users.noreply.github.com>

--- AutoTencent.py
+++ AutoTencent.py
@@ -1,84 +1,60 @@
-# utf-8
+# encoding: UTF-8
+from os.path import dirname, join
+
 from selenium import webdriver
-from selenium.webdriver.edge.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-import time,datetime
+from selenium.webdriver.support.ui import WebDriverWait
 
-# start=time.time()
-options =webdriver.ChromeOptions()
-# 设置EdgeOptions
-driver = webdriver.Chrome(options=options)
-# driver = webdriver.Edge()
-# 使用EdgeOptions初始化EdgeDriver
+URL = "https://docs.qq.com/form/page/DRnRDTklESlN4WERw"  # 在线收集表的 URL 地址
 
-url = "https//docs.qq.com/form/page/..."     #在线收集表的url地址
-driver.get(url)
+if __name__ == "__main__":
+    services = webdriver.EdgeService(
+        # 需要下载 EdgeDriver x64 版本，在项目根目录解压到 edgedriver_win64 目录下
+        join(dirname(__file__), "edgedriver_win64", "msedgedriver.exe")
+    )
+    with webdriver.Edge(service=services) as driver:
+        # 访问表单 URL
+        driver.get(URL)
 
+        WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.ID, "blankpage-button-pc"))
+        ).click()
+        WebDriverWait(driver, 15).until(
+            # 使用 QQ 互联登录
+            EC.presence_of_element_located((By.XPATH, "//span[@class='qq']"))
+        ).click()
 
-# 打开网址
-# time.sleep(1)
-content=''
-elmet = driver.find_element(By.ID,"header-login-btn")
-elmet.click()
-driver.implicitly_wait(2)
-# driver.execute_script("window.location.reload()")
-elmet=driver.find_element(By.CSS_SELECTOR,'span.qq')
-elmet.click()
-while True:    # 等待通过手机扫码或者其他方式登录，之后输入y即可开始等待开始抢填
-    ch = input("Are you logined ok?(y/n)")
-    if(ch == 'y'):
-        break
-# elmet = driver.find_element(By.ID,"img_out_qqnum")
-# elmet.click()
-# print("快捷登录成功")
-# time.sleep(1)
+        # 提供1分钟手动登录时长
+        element = WebDriverWait(driver, 120).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//textarea[@placeholder='请输入']")
+            )
+        )
 
-# 设定执行时间2023年9月20日14点
-execute_time = datetime.datetime(2023, 10, 20, 16, 38, 0)
+        # 操作链
+        (
+            ActionChains(driver)
+            .move_to_element(element)
+            .click()
+            .send_keys("啊对对队", Keys.TAB)
+            .send_keys("比利·海灵顿", Keys.TAB)
+            .send_keys("1145141919810", Keys.TAB)
+            .send_keys("田所浩二")
+            .send_keys(Keys.TAB, Keys.ENTER)
+            .perform()
+        )
+        WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//div[@class='dui-button-container' and text()='确认']")
+            )
+        ).click()
 
-# 计算需要等待的时间
-wait_time = execute_time - datetime.datetime.now()
-
-# 等待到指定时间再执行
-if wait_time.total_seconds() > 0:
-    print("Waiting for the beginning...")
-    time.sleep(wait_time.total_seconds())
-print("Beginning!")
-start = time.time()
-# 刷新网页
-driver.execute_script("window.location.reload()")
-
-# 等待元素出现
-timeout = 10  # 设置超时时间，单位为秒
-locator = (By.XPATH, "//textarea[@placeholder='请输入']")
-elements = []
-while not elements:
-    try:
-        elements = WebDriverWait(driver, timeout).until(EC.presence_of_all_elements_located(locator))
-    except:
-        pass
-elements[0].send_keys("11")   # 输入需要填写的信息：姓名、联系方式...
-elements[1].send_keys("22")
-elements[2].send_keys("33")
-elements[3].send_keys("44")
-
-# 由于不同的网页收集表提交和确认按钮似乎会变化，不推荐使用css_selector选择
-# button = driver.find_element(By.CSS_SELECTOR,'#root > div.form-root.fill-form-root > div > div > div.form-fill-container > div.form-with-history-record.fill-area > div.form-body.form-fill-body > div.question-commit > button')
-# driver.execute_script("arguments[0].click();", button)
-# elmet.click()
-# button = driver.find_element(By.CSS_SELECTOR,'body > div.dui-modal-mask.dui-modal-mask-visible.dui-modal-mask-display > div > div.dui-modal-footer > button.dui-button.dui-modal-footer-ok.dui-button-type-primary.dui-button-size-default > div')
-# button.click()
-# elmet = driver.find_element(By.CSS_SELECTOR,'textarea[placeholder="请输入"]')
-# elmet.send_keys("1562320xxxx")
-# textareas = driver.find_elements_by_css_selector('textarea[placeholder="请输入"]')
-
-button = driver.find_element(By.XPATH,"//button[text()='提交']")
-driver.execute_script("arguments[0].click();", button)
-locator = (By.XPATH, "//button[contains(.,'确认')]")
-button = WebDriverWait(driver, timeout).until(EC.presence_of_element_located(locator))
-button.click()
-
-print(time.time()-start)
-print("The current date and time is", datetime.datetime.now())
+        # 检查是否提交成功
+        WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//div[@type='form-submit-result-success']")
+            )
+        )
--- README.md
+++ README.md
@@ -1,8 +1,34 @@
-# Useful Scripts
-| File | Description | Remark |
-| ---- | ----------- | ------ |
-| AutoTencent.py | 腾讯文档在线收集表定时自动抢填脚本 | selenium环境依赖 |
-| TestTencent.py | 腾讯文档在线收集表选择题、填空题测试脚本 | selenium环境依赖 |
-|  ParseCpp.py   | cpp源代码文件简易解析脚本，可提取函数 | llvm, clang环境依赖, 见[tools](https://github.com/LiKe-rm/Useful-Scripts/tree/main/tools) |
-|  ImageCompress.py   | png/jpg图片定制尺寸压缩（极简）| - |
-|  SelfAttention.ipynb   | 细致拆解，pytorch实现自注意力、多头、交叉、因果（掩码）注意力 | pytorch |
+# 实用脚本
+
+| 文件名              | 说明                                                      |
+|---------------------|---------------------------------------------------------|
+| AutoTencent.py      | 腾讯文档在线收集表定时自动抢填脚本                        |
+| TestTencent.py      | 腾讯文档在线收集表选择题、填空题测试脚本                   |
+| ParseCpp.py         | C++ 源代码文件简易解析脚本，可提取函数                     |
+| ImageCompress.py    | PNG/JPG 图片定制尺寸压缩（极简）                            |
+| SelfAttention.ipynb | 细致拆解， PyTorch 实现自注意力、多头、交叉、因果（掩码）注意力 |
+
+## 使用方法
+
+```bash
+# 克隆仓库（SSH URL，推荐）
+git clone --progress git@github.com:Dragon1573/Useful-Scripts.git
+
+cd Useful-Scripts/
+
+# 创建虚拟环境
+python -m venv .venv
+
+# 激活虚拟环境
+./.venv/Scripts/activate
+
+# 升级 pip
+python -m pip install -U pip
+
+# 安装依赖项
+python -m pip install -U -r requirements.txt
+```
+
+## C++ 源代码文件解析脚本
+
+此文件依赖 LLVM ，请参考 [进一步说明](./tools/README.md) 。
